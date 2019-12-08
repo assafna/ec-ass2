d35bced4f4eba982a7629c5fb3c845886aa85053
Assaf <assafna@post.bgu.ac.il>
2019-12-08 18:34:11 +0200

    some more changes

--- nqueens_gp.py
+++ nqueens_gp.py
@@ -1,15 +1,17 @@
 import operator
+import statistics
 from functools import partial
 
 import multiprocessing
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 from deap import algorithms
 from deap import base
 from deap import creator
 from deap import tools
 from deap import gp
-
 
 NQUEENS_N = 20
 
@@ -196,9 +198,6 @@ def eval_nqueens(individual):
     routine = gp.compile(expr=individual, pset=pset)
     nqueens.run(routine)
     score = nqueens.eval_score2()
-    if score == 0:
-        nodes, edges, labels = gp.graph(individual)
-        # print_graph(edges, labels)
     return score,
 
 
@@ -210,12 +209,13 @@ toolbox.register('expr_mut', gp.genFull, min_=0, max_=2)
 toolbox.register('mutate', gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
 
 # bloat control
-toolbox.decorate('mate', gp.staticLimit(key=operator.attrgetter('height'), max_value=20))
-toolbox.decorate('mutate', gp.staticLimit(key=operator.attrgetter('height'), max_value=20))
+toolbox.decorate('mate', gp.staticLimit(key=operator.attrgetter('height'), max_value=17))
+toolbox.decorate('mutate', gp.staticLimit(key=operator.attrgetter('height'), max_value=17))
+
 
 # mp
-# pool = multiprocessing.Pool()
-# toolbox.register('map', pool.map)
+pool = multiprocessing.Pool()
+toolbox.register('map', pool.map)
 
 
 def print_graph(edges, labels):
@@ -223,20 +223,63 @@ def print_graph(edges, labels):
         print(str(edge[0]) + '.' + labels[edge[0]] + '->' + str(edge[1]) + '.' + labels[edge[1]])
 
 
-if __name__ == '__main__':
+def plot(_log1, _log2, _log3, _title):
+    data = pd.DataFrame(columns=('Generation', 'Best Fitness', 'Worst Fitness', 'Average Fitness', 'Median Fitness'))
+
+    gen = _log1.select('gen')
+    median1, median2, median3 = _log1.select('med'), _log2.select('med'), _log3.select('med')
+    avg1, avg2, avg3 = _log1.select('avg'), _log2.select('avg'), _log3.select('avg')
+    min1, min2, min3 = _log1.select('min'), _log2.select('min'), _log3.select('min')
+    max1, max2, max3 = _log1.select('max'), _log2.select('max'), _log3.select('max')
+
+    median = [statistics.mean(k) for k in zip(median1, median2, median3)]
+    avg = [statistics.mean(k) for k in zip(avg1, avg2, avg3)]
+    min_ = [statistics.mean(k) for k in zip(min1, min2, min3)]
+    max_ = [statistics.mean(k) for k in zip(max1, max2, max3)]
+
+    data['Generation'] = gen
+    data['Best Fitness'] = min_
+    data['Worst Fitness'] = max_
+    data['Average Fitness'] = avg
+    data['Median Fitness'] = median
+
+    data.to_csv(_title + '.csv')
+
+    fig, ax1 = plt.subplots()
+    line1 = ax1.plot(gen, min_, 'b-', label='Best Fitness')
+    ax1.set(xlabel='Generation', ylabel='Fitness',
+            title=_title + ' Avg Results')
+    ax1.grid()
+    line2 = ax1.plot(gen, max_, 'r-', label='Worst Fitness')
+    line3 = ax1.plot(gen, avg, 'g-', label='Average Fitness')
+    line4 = ax1.plot(gen, median, 'y-', label='Median Fitness')
+    lns = line1 + line2 + line3 + line4
+    labs = [l.get_label() for l in lns]
+    ax1.legend(lns, labs, loc='best')
+    plt.show()
+
+
+def run():
     pop = toolbox.population(n=1000)
     hof = tools.HallOfFame(1)
     stats = tools.Statistics(lambda ind: ind.fitness.values)
     stats.register('avg', np.mean)
-    stats.register('std', np.std)
+    stats.register('med', np.median)
     stats.register('min', np.min)
     stats.register('max', np.max)
 
-    algorithms.eaSimple(population=pop,
-                        toolbox=toolbox,
-                        cxpb=0.9,
-                        mutpb=0.2,
-                        ngen=200,
-                        stats=stats,
-                        halloffame=hof
-                        )
+    _, log = algorithms.eaSimple(population=pop, toolbox=toolbox, cxpb=0.9, mutpb=0.2, ngen=200, stats=stats,
+                                  halloffame=hof)
+
+    return log, hof
+
+
+if __name__ == '__main__':
+    log1, hof1 = run()
+    log2, hof2 = run()
+    log3, hof3 = run()
+
+    plot(log1, log2, log3, _title='N=8')
+
+    nodes, edges, labels = gp.graph(hof3[0])
+    print_graph(edges, labels)
